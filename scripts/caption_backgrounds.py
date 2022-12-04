a810cc53be874092568b41f06b362066b2172edd
JHNam <harryjhnam@gm.gist.ac.kr>
2022-12-04 15:51:36 +0900

    add model architecture argument

--- scripts/caption_backgrounds.py
+++ scripts/caption_backgrounds.py
@@ -3,9 +3,10 @@ from glob import glob
 from subprocess import check_call
 
 background_paths = glob('data/backgrounds/30_place_dataset/*/*.jpg')
-output_paths = [ 'data/backgrounds/30_place_dataset_vgg_caption/' + '/'.join(path.split('/')[-2:])
+output_paths = [ 'data/backgrounds/30_place_dataset_resnet_caption/' + '/'.join(path.split('/')[-2:])
                 for path in background_paths]
-model_ckpt_path = 'data/pretrained/VGG19_decoder.pth'
+model_type = 'resnet152'
+model_ckpt_path = 'data/pretrained/ResNet152_decoder_teacherforcing.pth'
 
 PYTHON = sys.executable
 
@@ -14,6 +15,6 @@ for background_path, output_path in zip(background_paths, output_paths):
     if not os.path.exists('/'.join(output_path.split('/')[:-1])):
         os.makedirs('/'.join(output_path.split('/')[:-1]))
 
-    cmd = f"{PYTHON} generate_caption.py --img-path {background_path} --model {model_ckpt_path} --output-img-path {output_path}"
+    cmd = f"{PYTHON} generate_caption.py --img-path {background_path} --network {model_type} --model {model_ckpt_path} --output-img-path {output_path}"
     print(cmd)
     check_call(cmd, shell=True)
