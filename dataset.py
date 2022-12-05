9bee13a0339e88b4d475f2c391f8c0802c27858e
JHNam <harryjhnam@gm.gist.ac.kr>
2022-12-05 16:24:15 +0900

    fix bugs in dataset and add dataset usage demo ipynb file

--- dataset.py
+++ dataset.py
@@ -16,7 +16,7 @@ def pil_loader(path):
 
 class ImagePairedCaptionDataset(Dataset):
     def __init__(self, transform, images_dir, annotations_dir, word_dict, split_type='train'):
-        super(ImageCaptionDataset, self).__init__()
+        super(ImagePairedCaptionDataset, self).__init__()
         self.split_type = split_type
         self.transform = transform
 
@@ -114,7 +114,7 @@ class ImagePairedCaptionDataset(Dataset):
         if self.split_type == 'train':
             return (torch.FloatTensor(img1), caption1),\
                    (torch.FloatTensor(img2), caption2),\
-                   CLUB_mask, InfoNCE_mask
+                   CLUB_mask.tolist(), InfoNCE_mask.tolist()
 
         elif self.split_type == 'val':
             all_captions1 = [ [self.word2idx['<sos>']] + [self.word2idx[word] for word in caption.split(' ')] + [self.word2idx['<eos>']]
@@ -123,7 +123,7 @@ class ImagePairedCaptionDataset(Dataset):
                             for caption in self.image2captions[img_path2]]
             return (torch.FloatTensor(img1), caption1, all_captions1),\
                    (torch.FloatTensor(img2), caption2, all_captions2),\
-                   CLUB_mask, InfoNCE_mask
+                   CLUB_mask.tolist(), InfoNCE_mask.tolist()
 
     def __len__(self):
         return len(self.img_path_pairs)
@@ -132,11 +132,15 @@ class ImagePairedCaptionDataset(Dataset):
     def collate_fn(self, data):
 
         if self.split_type == 'train':
-            (img1s, caption1s), (img2s, caption2s), target_word_masks, cls_template_masks = zip(*data)
+            img_cap_1s, img_cap_2s, target_word_masks, cls_template_masks = zip(*data)
+            img1s, caption1s = zip(*img_cap_1s)
+            img2s, caption2s = zip(*img_cap_2s)
             max_caption_len = max( max([len(caption1) for caption1 in caption1s]), max([len(caption2) for caption2 in caption2s]) )
 
         elif self.split_type == 'val':
-            (img1s, caption1s, all_captions1s), (img2s, caption2s, all_captions2s), target_word_masks, cls_template_masks = zip(*data)
+            img_cap_1s, img_cap_2s, target_word_masks, cls_template_masks = zip(*data)
+            img1s, caption1s, all_captions1s = zip(*img_cap_1s)
+            img2s, caption2s, all_captions2s = zip(*img_cap_2s)
             max_caption_len = 0
             for all_captions in all_captions1s + all_captions2s:
                 max_caption_len = max( max_caption_len, max([len(caption) for caption in all_captions]) )
@@ -147,8 +151,8 @@ class ImagePairedCaptionDataset(Dataset):
         cls_template_masks = [mask + [0] * (max_caption_len - len(mask)) for mask in cls_template_masks]
 
         if self.split_type == 'train':
-            return torch.stack(img1s), torch.LongTensor(caption1s),\
-                   torch.stack(img2s), torch.LongTensor(caption2s),\
+            return ( torch.stack(img1s), torch.LongTensor(caption1s) ),\
+                   ( torch.stack(img2s), torch.LongTensor(caption2s) ),\
                    torch.LongTensor(target_word_masks), torch.LongTensor(cls_template_masks)
 
         elif self.split_type == 'val':
@@ -157,8 +161,8 @@ class ImagePairedCaptionDataset(Dataset):
             all_captions2s = [ [caption + [self.pad_idx] * (max_caption_len - len(caption)) for caption in all_captions2]
                             for all_captions2 in all_captions2s ]
 
-            return torch.stack(img1s), torch.LongTensor(caption1s), torch.LongTensor(all_captions1s),\
-                   torch.stack(img2s), torch.LongTensor(caption2s), torch.LongTensor(all_captions2s),\
+            return ( torch.stack(img1s), torch.LongTensor(caption1s), torch.LongTensor(all_captions1s) ),\
+                   ( torch.stack(img2s), torch.LongTensor(caption2s), torch.LongTensor(all_captions2s) ),\
                    torch.LongTensor(target_word_masks), torch.LongTensor(cls_template_masks)
 
 
