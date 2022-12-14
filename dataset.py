code for testing trained models on generated dataset and 100 selected original dataset; also fix average accuracy step mistake

dataset.py
@@ -6,12 +6,19 @@ from torch.utils.data import Dataset
 
 from PIL import Image
 
+import string
+
 
 
 def pil_loader(path):
     with open(path, 'rb') as f:
         img = Image.open(f)
         return img.convert('RGB')
+
+def preprocess_caption(caption):
+    caption = caption.lower()
+    caption = ''.join( [c for c in caption if c not in string.punctuation] )
+    return caption
 
 
 class ImagePairedCaptionDataset(Dataset):
@@ -117,9 +124,9 @@ class ImagePairedCaptionDataset(Dataset):
                    CLUB_mask.long().tolist(), InfoNCE_mask.long().tolist()
 
         elif self.split_type in ['val', 'test']:
-            all_captions1 = [ [self.word2idx['<sos>']] + [self.word2idx[word] for word in caption.split(' ')] + [self.word2idx['<eos>']]
+            all_captions1 = [ [self.word2idx['<start>']] + [self.word2idx[word] for word in caption.split(' ')] + [self.word2idx['<eos>']]
                             for caption in self.image2captions[img_path1]]
-            all_captions2 = [ [self.word2idx['<sos>']] + [self.word2idx[word] for word in caption.split(' ')] + [self.word2idx['<eos>']]
+            all_captions2 = [ [self.word2idx['<start>']] + [self.word2idx[word] for word in caption.split(' ')] + [self.word2idx['<eos>']]
                             for caption in self.image2captions[img_path2]]
             return (torch.FloatTensor(img1), caption1, all_captions1),\
                    (torch.FloatTensor(img2), caption2, all_captions2),\
@@ -178,5 +185,52 @@ class ImagePairedCaptionDataset(Dataset):
 
 
 
-class ImageCaptionDataset(Dataset):
-    pass
+class ImageCaptionTestDataset(Dataset):
+    def __init__(self, transform, coco_data_path, word_dict, split_type='test'):
+        super(ImageCaptionTestDataset, self).__init__()
+
+        assert split_type == 'test', "This dataset if only for testing."
+
+        self.split_type = split_type
+        self.transform = transform
+        self.word2idx = defaultdict(lambda: word_dict['<unk>'])
+        self.word2idx.update(word_dict)
+        self.pad_idx = self.word2idx['<pad>']
+
+        self.images_dir = os.path.join(coco_data_path, 'images/')
+
+        self.caption_img_idx = {}
+        self.img_paths = json.load(open(coco_data_path + '/{}_img_paths.json'.format(split_type), 'r'))
+        
+        captions = json.load(open(coco_data_path + '/{}_captions.json'.format(split_type), 'r'))
+        captions = [ preprocess_caption(caption) for caption in captions]
+
+        self.captions = []
+        for caption in captions:
+            self.captions.append( [self.word2idx['<start>']] + [ self.word2idx[c] for c in caption.split(' ') ] + [self.word2idx['<eos>']] )
+
+    def __getitem__(self, index):
+        img_path = self.img_paths[index]
+        img = pil_loader(os.path.join(self.images_dir, img_path))
+        if self.transform is not None:
+            img = self.transform(img)
+
+        matching_idxs = [idx for idx, path in enumerate(self.img_paths) if path == img_path]
+        all_captions = [self.captions[idx] for idx in matching_idxs]
+
+        return torch.FloatTensor(img), self.captions[index], all_captions
+
+    def __len__(self):
+        return len(self.captions)
+
+    def collate_fn(self, data):
+
+        imgs, captions, all_captionss = zip(*data)
+        max_caption_len = max([ max([len(caption) for caption in all_captions]) for all_captions in all_captionss ])
+        
+        padded_captions = [ caption + [self.pad_idx]*(max_caption_len - len(caption)) for caption in captions ]
+        padded_all_captionss = []
+        for all_captions in all_captionss:
+            padded_all_captionss.append( [ caption + [self.pad_idx]*(max_caption_len - len(caption)) for caption in all_captions ] )
+
+        return ( torch.stack(imgs), torch.LongTensor(padded_captions), torch.LongTensor(padded_all_captionss) )

test_COCO.py
@@ -13,7 +13,7 @@ from torchvision import transforms
 from torch.utils.data import DataLoader
 
 from mi_estimators import *
-from dataset import ImagePairedCaptionDataset
+from dataset import ImageCaptionTestDataset
 from decoder import Decoder
 from encoder import Encoder
 from utils import AverageMeter, accuracy, calculate_caption_lengths
@@ -35,97 +35,63 @@ def test(encoder, decoder, data_loader, word_dict, log_interval):
     top5 = AverageMeter()
 
     # used for calculating bleu scores
-    references1 = []
-    hypotheses1 = []
-    references2 = []
-    hypotheses2 = []
+    references = []
+    hypotheses = []
 
     with torch.no_grad():
-        for batch_idx, ((img1, cap1, all_cap1), (img2, cap2, all_cap2), _, _) in enumerate(data_loader):
+        for batch_idx, (images, captions, all_captions) in enumerate(data_loader):
 
-            img1 = Variable(img1.cuda())
-            img2 = Variable(img2.cuda())
-            cap1 = Variable(cap1.cuda())
-            cap2 = Variable(cap2.cuda())
+            images = Variable(images.cuda())
+            captions = Variable(captions.cuda())
 
             # forward
-            features1 = encoder(img1)
-            features2 = encoder(img2)
-            preds1, _, _ = decoder(features1, cap1)
-            preds2, _, _ = decoder(features2, cap2)
+            features = encoder(images)
+            preds, _, _ = decoder(features, captions)
 
             # calculate caption lengths
             pad_idx = word_dict['<pad>']
-            target_lens1 = (cap1.clone().detach().long()!=pad_idx).sum(dim=1)
-            target_lens2 = (cap2.clone().detach().long()!=pad_idx).sum(dim=1)
+            target_lens = (captions.clone().detach().long()!=pad_idx).sum(dim=1)
 
             # calculate accuracy
-            targets1 = cap1[:, 1:].clone()
-            targets2 = cap2[:, 1:].clone()
-            acc1_1 = accuracy(preds1, targets1, target_lens1, 1)
-            acc1_5 = accuracy(preds1, targets1, target_lens1, 5)
-            acc2_1 = accuracy(preds2, targets2, target_lens2, 1)
-            acc2_5 = accuracy(preds2, targets2, target_lens2, 5)
+            targets = captions[:, 1:].clone()
+            acc1 = accuracy(preds, targets, target_lens, 1)
+            acc5 = accuracy(preds, targets, target_lens, 5)
 
-            total_caption_length1 = calculate_caption_lengths(word_dict, cap1)
-            total_caption_length2 = calculate_caption_lengths(word_dict, cap2)
+            total_caption_length = calculate_caption_lengths(word_dict, captions)
 
-            top1.update((acc1_1 + acc2_1)/2, total_caption_length1)
-            top5.update((acc1_5 + acc2_5)/2, total_caption_length2)
+            top1.update(acc1, total_caption_length)
+            top5.update(acc5, total_caption_length)
 
             # calculate bleu scores
-            for cap_set in all_cap1.tolist():
-                caps1 = []
+            for cap_set in all_captions.tolist():
+                caps = []
                 for caption in cap_set:
                     cap = [word_idx for word_idx in caption
                                     if word_idx != word_dict['<start>'] and word_idx != word_dict['<pad>']]
-                    caps1.append(cap)
-                references1.append(caps1)
+                    caps.append(cap)
+                references.append(caps)
 
-            for cap_set in all_cap2.tolist():
-                caps2 = []
-                for caption in cap_set:
-                    cap = [word_idx for word_idx in caption
-                                    if word_idx != word_dict['<start>'] and word_idx != word_dict['<pad>']]
-                    caps2.append(cap)
-                references2.append(caps2)
-
-            word_idxs1 = torch.max(preds1, dim=2)[1]
-            for idxs in word_idxs1.tolist():
-                hypotheses1.append([idx for idx in idxs
+            word_idxs = torch.max(preds, dim=2)[1]
+            for idxs in word_idxs.tolist():
+                hypotheses.append([idx for idx in idxs
                                        if idx != word_dict['<start>'] and idx != word_dict['<pad>']])
 
-            word_idxs2 = torch.max(preds2, dim=2)[1]
-            for idxs in word_idxs2.tolist():
-                hypotheses2.append([idx for idx in idxs
-                                       if idx != word_dict['<start>'] and idx != word_dict['<pad>']])
             if batch_idx % log_interval == 0:     
                 print('Test Batch: [{0}/{1}]\t'
                       'Top 1 Accuracy {top1.val:.3f} ({top1.avg:.3f})\t'
                       'Top 5 Accuracy {top5.val:.3f} ({top5.avg:.3f})'.format(
                           batch_idx, len(data_loader), top1=top1, top5=top5))
 
-        bleu_1_1 = corpus_bleu(references1, hypotheses1, weights=(1, 0, 0, 0))
-        bleu_1_2 = corpus_bleu(references1, hypotheses1, weights=(0.5, 0.5, 0, 0))
-        bleu_1_3 = corpus_bleu(references1, hypotheses1, weights=(0.33, 0.33, 0.33, 0))
-        bleu_1_4 = corpus_bleu(references1, hypotheses1)
-
-        bleu_2_1 = corpus_bleu(references2, hypotheses2, weights=(1, 0, 0, 0))
-        bleu_2_2 = corpus_bleu(references2, hypotheses2, weights=(0.5, 0.5, 0, 0))
-        bleu_2_3 = corpus_bleu(references2, hypotheses2, weights=(0.33, 0.33, 0.33, 0))
-        bleu_2_4 = corpus_bleu(references2, hypotheses2)
+        bleu_1 = corpus_bleu(references, hypotheses, weights=(1, 0, 0, 0))
+        bleu_2 = corpus_bleu(references, hypotheses, weights=(0.5, 0.5, 0, 0))
+        bleu_3 = corpus_bleu(references, hypotheses, weights=(0.33, 0.33, 0.33, 0))
+        bleu_4 = corpus_bleu(references, hypotheses)
 
         print('Testing :\t'
                 'BLEU-1 ({})\t'
                 'BLEU-2 ({})\t'
                 'BLEU-3 ({})\t'
-                'BLEU-4 ({})\t'.format(bleu_1_1, bleu_1_2, bleu_1_3, bleu_1_4))
-        
-        print('Testing :\t'  
-                'BLEU-1 ({})\t'
-                'BLEU-2 ({})\t'
-                'BLEU-3 ({})\t'
-                'BLEU-4 ({})\t'.format(bleu_2_1, bleu_2_2, bleu_2_3, bleu_2_4))
+                'BLEU-4 ({})\t'.format(bleu_1, bleu_2, bleu_3, bleu_4))
 
 
 
@@ -136,7 +102,7 @@ def main(args):
     vocabulary_size = len(word_dict)
     
     encoder = Encoder(args.network)
-    decoder = Decoder(vocabulary_size, encoder.dim, args.tf)
+    decoder = Decoder(vocabulary_size, encoder.dim, args.tf, args.use_filter)
 
     if args.use_filter:
         print(f'Load pretrained weights for decoder from {args.model}')
@@ -154,7 +120,7 @@ def main(args):
     encoder.cuda()
     decoder.cuda()
 
-    test_dataset = ImagePairedCaptionDataset(transform=data_transforms, images_dir=args.images_dir, annotations_dir=args.annotations_dir, word_dict=word_dict, split_type='test')
+    test_dataset = ImageCaptionTestDataset(transform=data_transforms, coco_data_path=args.data, word_dict=word_dict, split_type='test')
     
     test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4, pin_memory=True, collate_fn=test_dataset.collate_fn)
 
@@ -169,8 +135,6 @@ if __name__ == "__main__":
                         help='number of batches to wait before logging training stats (default: 100)')
     parser.add_argument('--data', type=str, default='data/coco',
                         help='path to data images (default: data/coco)')
-    parser.add_argument('--images-dir', type=str, default='./data/final/')
-    parser.add_argument('--annotations-dir', type=str, default='./data/annotations/')
     parser.add_argument('--network', choices=['vgg19', 'resnet152', 'densenet161'], default='vgg19',
                         help='Network to use in the encoder (default: vgg19)')
     parser.add_argument('--model', type=str, default='data/pretrained/VGG19_decoder.pth',